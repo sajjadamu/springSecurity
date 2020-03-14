78625e14f9396f348890e076b42d37cabf67705f
Sajjad <sajjad.hussain@indicsoft.com>
2020-03-14 21:21:30 +0530

    comment  annotation repository

--- src/main/java/io/javabrains/springsecurityjpaImp/UserRepository.java
+++ src/main/java/io/javabrains/springsecurityjpaImp/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import model.User;
 
-@Repository
+//@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 	
 	Optional<User> findByUsername(String username);
