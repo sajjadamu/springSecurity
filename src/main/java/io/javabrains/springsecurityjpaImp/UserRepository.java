c63b09dbdf40b12edff849416f7f12d1ce321205
Sajjad <sajjad.hussain@indicsoft.com>
2020-03-14 21:15:40 +0530

    add annotation repository

--- src/main/java/io/javabrains/springsecurityjpaImp/UserRepository.java
+++ src/main/java/io/javabrains/springsecurityjpaImp/UserRepository.java
@@ -3,10 +3,11 @@ package io.javabrains.springsecurityjpaImp;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import model.User;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 	
 	Optional<User> findByUsername(String username);
